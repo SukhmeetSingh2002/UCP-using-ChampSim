1755dd0b8773d63fbf968f6fa7db9aa03ec64252
Vishnusai-Janjanam <v.sai.j2025@gmail.com>
2022-05-11 00:35:18 +0530

    Update cache.h

--- inc/cache.h
+++ inc/cache.h
@@ -10,10 +10,10 @@ extern uint32_t PAGE_TABLE_LATENCY, SWAP_LATENCY;
 #define IS_ITLB 0
 #define IS_DTLB 1
 #define IS_STLB 2
-#define IS_L1I 3
-#define IS_L1D 4
-#define IS_L2C 5
-#define IS_LLC 6
+#define IS_L1I  3
+#define IS_L1D  4
+#define IS_L2C  5
+#define IS_LLC  6
 
 // INSTRUCTION TLB
 #define ITLB_SET 16
@@ -46,7 +46,7 @@ extern uint32_t PAGE_TABLE_LATENCY, SWAP_LATENCY;
 #define L1I_SET 64
 #define L1I_WAY 8
 #define L1I_RQ_SIZE 64
-#define L1I_WQ_SIZE 64
+#define L1I_WQ_SIZE 64 
 #define L1I_PQ_SIZE 32
 #define L1I_MSHR_SIZE 8
 #define L1I_LATENCY 4
@@ -55,10 +55,10 @@ extern uint32_t PAGE_TABLE_LATENCY, SWAP_LATENCY;
 #define L1D_SET 64
 #define L1D_WAY 12
 #define L1D_RQ_SIZE 64
-#define L1D_WQ_SIZE 64
+#define L1D_WQ_SIZE 64 
 #define L1D_PQ_SIZE 8
 #define L1D_MSHR_SIZE 16
-#define L1D_LATENCY 5
+#define L1D_LATENCY 5 
 
 // L2 CACHE
 #define L2C_SET 1024
@@ -67,20 +67,19 @@ extern uint32_t PAGE_TABLE_LATENCY, SWAP_LATENCY;
 #define L2C_WQ_SIZE 32
 #define L2C_PQ_SIZE 16
 #define L2C_MSHR_SIZE 32
-#define L2C_LATENCY 10 // 4/5 (L1I or L1D) + 10 = 14/15 cycles
+#define L2C_LATENCY 10  // 4/5 (L1I or L1D) + 10 = 14/15 cycles
 
 // LAST LEVEL CACHE
-#define LLC_SET NUM_CPUS * 2048
+#define LLC_SET NUM_CPUS*2048
 #define LLC_WAY 16
-#define LLC_RQ_SIZE NUM_CPUS *L2C_MSHR_SIZE // 48
-#define LLC_WQ_SIZE NUM_CPUS *L2C_MSHR_SIZE // 48
-#define LLC_PQ_SIZE NUM_CPUS * 32
-#define LLC_MSHR_SIZE NUM_CPUS * 64
-#define LLC_LATENCY 20 // 4/5 (L1I or L1D) + 10 + 20 = 34/35 cycles
+#define LLC_RQ_SIZE NUM_CPUS*L2C_MSHR_SIZE //48
+#define LLC_WQ_SIZE NUM_CPUS*L2C_MSHR_SIZE //48
+#define LLC_PQ_SIZE NUM_CPUS*32
+#define LLC_MSHR_SIZE NUM_CPUS*64
+#define LLC_LATENCY 20  // 4/5 (L1I or L1D) + 10 + 20 = 34/35 cycles
 
-class CACHE : public MEMORY
-{
-public:
+class CACHE : public MEMORY {
+  public:
     uint32_t cpu;
     const string NAME;
     const uint32_t NUM_SET, NUM_WAY, NUM_LINE, WQ_SIZE, RQ_SIZE, PQ_SIZE, MSHR_SIZE;
@@ -93,51 +92,44 @@ public:
 
     // prefetch stats
     uint64_t pf_requested,
-        pf_issued,
-        pf_useful,
-        pf_useless,
-        pf_fill;
+             pf_issued,
+             pf_useful,
+             pf_useless,
+             pf_fill;
 
     // queues
-    PACKET_QUEUE WQ{NAME + "_WQ", WQ_SIZE},       // write queue
-        RQ{NAME + "_RQ", RQ_SIZE},                // read queue
-        PQ{NAME + "_PQ", PQ_SIZE},                // prefetch queue
-        MSHR{NAME + "_MSHR", MSHR_SIZE},          // MSHR
-        PROCESSED{NAME + "_PROCESSED", ROB_SIZE}; // processed queue
+    PACKET_QUEUE WQ{NAME + "_WQ", WQ_SIZE}, // write queue
+                 RQ{NAME + "_RQ", RQ_SIZE}, // read queue
+                 PQ{NAME + "_PQ", PQ_SIZE}, // prefetch queue
+                 MSHR{NAME + "_MSHR", MSHR_SIZE}, // MSHR
+                 PROCESSED{NAME + "_PROCESSED", ROB_SIZE}; // processed queue
 
     uint64_t sim_access[NUM_CPUS][NUM_TYPES],
-        sim_hit[NUM_CPUS][NUM_TYPES],
-        sim_miss[NUM_CPUS][NUM_TYPES],
-        roi_access[NUM_CPUS][NUM_TYPES],
-        roi_hit[NUM_CPUS][NUM_TYPES],
-        roi_miss[NUM_CPUS][NUM_TYPES];
+             sim_hit[NUM_CPUS][NUM_TYPES],
+             sim_miss[NUM_CPUS][NUM_TYPES],
+             roi_access[NUM_CPUS][NUM_TYPES],
+             roi_hit[NUM_CPUS][NUM_TYPES],
+             roi_miss[NUM_CPUS][NUM_TYPES];
 
     uint64_t total_miss_latency;
-
+    
     // constructor
-    CACHE(string v1, uint32_t v2, int v3, uint32_t v4, uint32_t v5, uint32_t v6, uint32_t v7, uint32_t v8)
-        : NAME(v1), NUM_SET(v2), NUM_WAY(v3), NUM_LINE(v4), WQ_SIZE(v5), RQ_SIZE(v6), PQ_SIZE(v7), MSHR_SIZE(v8)
-    {
+    CACHE(string v1, uint32_t v2, int v3, uint32_t v4, uint32_t v5, uint32_t v6, uint32_t v7, uint32_t v8) 
+        : NAME(v1), NUM_SET(v2), NUM_WAY(v3), NUM_LINE(v4), WQ_SIZE(v5), RQ_SIZE(v6), PQ_SIZE(v7), MSHR_SIZE(v8) {
 
         LATENCY = 0;
 
         // cache block
-        block = new BLOCK *[NUM_SET];
-        for (uint32_t i = 0; i < NUM_SET; i++)
-        {
-            block[i] = new BLOCK[NUM_WAY];
-
-            for (uint32_t j = 0; j < NUM_WAY; j++)
-            {
-                if (v1 == "LLC" and j < 15)
+        block = new BLOCK* [NUM_SET];
+        uint32_t DIVIDER = LLC_WAY/NUM_CPUS;
+        for (uint32_t i=0; i<NUM_SET; i++) {
+            block[i] = new BLOCK[NUM_WAY]; 
+            
+            for (uint32_t j=0; j<NUM_WAY; j++) {
+                if(v1=="LLC")
                 {
-                    block[i][j].lru = j;
-                    block[i][j].cpu = 0;
-                }
-                else if (v1 == "LLC" and j >= 15)
-                {
-                    block[i][j].lru = j - 15;
-                    block[i][j].cpu = 1;
+                    block[i][j].lru = j%DIVIDER;
+                    block[i][j].cpu = j/DIVIDER;
                 }
                 else
                 {
@@ -145,14 +137,11 @@ public:
                 }
             }
         }
-
-        for (uint32_t i = 0; i < NUM_CPUS; i++)
-        {
+        for (uint32_t i=0; i<NUM_CPUS; i++) {
             upper_level_icache[i] = NULL;
             upper_level_dcache[i] = NULL;
 
-            for (uint32_t j = 0; j < NUM_TYPES; j++)
-            {
+            for (uint32_t j=0; j<NUM_TYPES; j++) {
                 sim_access[i][j] = 0;
                 sim_hit[i][j] = 0;
                 sim_miss[i][j] = 0;
@@ -162,7 +151,7 @@ public:
             }
         }
 
-        total_miss_latency = 0;
+	total_miss_latency = 0;
 
         lower_level = NULL;
         extra_interface = NULL;
@@ -178,81 +167,76 @@ public:
     };
 
     // destructor
-    ~CACHE()
-    {
-        for (uint32_t i = 0; i < NUM_SET; i++)
+    ~CACHE() {
+        for (uint32_t i=0; i<NUM_SET; i++)
             delete[] block[i];
         delete[] block;
     };
 
     // functions
-    int add_rq(PACKET *packet),
-        add_wq(PACKET *packet),
-        add_pq(PACKET *packet);
+    int  add_rq(PACKET *packet),
+         add_wq(PACKET *packet),
+         add_pq(PACKET *packet);
 
     void return_data(PACKET *packet),
-        operate(),
-        increment_WQ_FULL(uint64_t address);
+         operate(),
+         increment_WQ_FULL(uint64_t address);
 
     uint32_t get_occupancy(uint8_t queue_type, uint64_t address),
-        get_size(uint8_t queue_type, uint64_t address);
+             get_size(uint8_t queue_type, uint64_t address);
 
-    int check_hit(PACKET *packet),
-        invalidate_entry(uint64_t inval_addr),
-        check_mshr(PACKET *packet),
-        prefetch_line(uint64_t ip, uint64_t base_addr, uint64_t pf_addr, int prefetch_fill_level, uint32_t prefetch_metadata),
-        kpc_prefetch_line(uint64_t base_addr, uint64_t pf_addr, int prefetch_fill_level, int delta, int depth, int signature, int confidence, uint32_t prefetch_metadata);
+    int  check_hit(PACKET *packet),
+         invalidate_entry(uint64_t inval_addr),
+         check_mshr(PACKET *packet),
+         prefetch_line(uint64_t ip, uint64_t base_addr, uint64_t pf_addr, int prefetch_fill_level, uint32_t prefetch_metadata),
+         kpc_prefetch_line(uint64_t base_addr, uint64_t pf_addr, int prefetch_fill_level, int delta, int depth, int signature, int confidence, uint32_t prefetch_metadata);
 
     void handle_fill(),
-        handle_writeback(),
-        handle_read(),
-        handle_prefetch();
+         handle_writeback(),
+         handle_read(),
+         handle_prefetch();
 
     void add_mshr(PACKET *packet),
-        update_fill_cycle(),
-        llc_initialize_replacement(),
-        update_replacement_state(uint32_t cpu, uint32_t set, uint32_t way, uint64_t full_addr, uint64_t ip, uint64_t victim_addr, uint32_t type, uint8_t hit),
-        llc_update_replacement_state(uint32_t cpu, uint32_t set, uint32_t way, uint64_t full_addr, uint64_t ip, uint64_t victim_addr, uint32_t type, uint8_t hit),
-        lru_update(uint32_t set, uint32_t way, uint32_t cpu),
-        lru_update_notLLC(uint32_t set, uint32_t way),
-        fill_cache(uint32_t set, uint32_t way, PACKET *packet),
-        replacement_final_stats(),
-        llc_replacement_final_stats(),
-        // prefetcher_initialize(),
-        l1d_prefetcher_initialize(),
-        l2c_prefetcher_initialize(),
-        llc_prefetcher_initialize(),
-        prefetcher_operate(uint64_t addr, uint64_t ip, uint8_t cache_hit, uint8_t type),
-        l1d_prefetcher_operate(uint64_t addr, uint64_t ip, uint8_t cache_hit, uint8_t type),
-        prefetcher_cache_fill(uint64_t addr, uint32_t set, uint32_t way, uint8_t prefetch, uint64_t evicted_addr),
-        l1d_prefetcher_cache_fill(uint64_t addr, uint32_t set, uint32_t way, uint8_t prefetch, uint64_t evicted_addr, uint32_t metadata_in),
-        // prefetcher_final_stats(),
-        l1d_prefetcher_final_stats(),
-        l2c_prefetcher_final_stats(),
-        UMON_update(uint32_t cpu, uint32_t set_index, uint32_t way),
-        UMON(uint32_t cpu, uint64_t full_addr, uint32_t set),
-        make_partition(),
-        ucp_update(uint32_t set, uint32_t way, uint32_t cpu),
-        llc_prefetcher_final_stats();
-
+         update_fill_cycle(),
+         llc_initialize_replacement(),
+         update_replacement_state(uint32_t cpu, uint32_t set, uint32_t way, uint64_t full_addr, uint64_t ip, uint64_t victim_addr, uint32_t type, uint8_t hit),
+         llc_update_replacement_state(uint32_t cpu, uint32_t set, uint32_t way, uint64_t full_addr, uint64_t ip, uint64_t victim_addr, uint32_t type, uint8_t hit),
+         lru_update(uint32_t set, uint32_t way),
+         fill_cache(uint32_t set, uint32_t way, PACKET *packet),
+         replacement_final_stats(),
+         llc_replacement_final_stats(),
+         //prefetcher_initialize(),
+         l1d_prefetcher_initialize(),
+         l2c_prefetcher_initialize(),
+         llc_prefetcher_initialize(),
+         prefetcher_operate(uint64_t addr, uint64_t ip, uint8_t cache_hit, uint8_t type),
+         l1d_prefetcher_operate(uint64_t addr, uint64_t ip, uint8_t cache_hit, uint8_t type),
+         prefetcher_cache_fill(uint64_t addr, uint32_t set, uint32_t way, uint8_t prefetch, uint64_t evicted_addr),
+         l1d_prefetcher_cache_fill(uint64_t addr, uint32_t set, uint32_t way, uint8_t prefetch, uint64_t evicted_addr, uint32_t metadata_in),
+         //prefetcher_final_stats(),
+         l1d_prefetcher_final_stats(),
+         l2c_prefetcher_final_stats(),
+         llc_prefetcher_final_stats();
     void (*l1i_prefetcher_cache_operate)(uint32_t, uint64_t, uint8_t, uint8_t);
     void (*l1i_prefetcher_cache_fill)(uint32_t, uint64_t, uint32_t, uint32_t, uint8_t, uint64_t);
 
     uint32_t l2c_prefetcher_operate(uint64_t addr, uint64_t ip, uint8_t cache_hit, uint8_t type, uint32_t metadata_in),
-        llc_prefetcher_operate(uint64_t addr, uint64_t ip, uint8_t cache_hit, uint8_t type, uint32_t metadata_in),
-        l2c_prefetcher_cache_fill(uint64_t addr, uint32_t set, uint32_t way, uint8_t prefetch, uint64_t evicted_addr, uint32_t metadata_in),
-        llc_prefetcher_cache_fill(uint64_t addr, uint32_t set, uint32_t way, uint8_t prefetch, uint64_t evicted_addr, uint32_t metadata_in);
-
-    uint32_t get_set(uint64_t address),
-        get_way(uint64_t address, uint32_t set),
-        find_victim(uint32_t cpu, uint64_t instr_id, uint32_t set, const BLOCK *current_set, uint64_t ip, uint64_t full_addr, uint32_t type),
-        llc_find_victim(uint32_t cpu, uint64_t instr_id, uint32_t set, const BLOCK *current_set, uint64_t ip, uint64_t full_addr, uint32_t type),
-        lru_victim(uint32_t cpu, uint64_t instr_id, uint32_t set, const BLOCK *current_set, uint64_t ip, uint64_t full_addr, uint32_t type),
-        UMON_find_victim(uint32_t cpu, uint32_t set_index),
-        lru_victim_notLLC(uint32_t cpu, uint64_t instr_id, uint32_t set, const BLOCK *current_set, uint64_t ip, uint64_t full_addr, uint32_t type);
+         llc_prefetcher_operate(uint64_t addr, uint64_t ip, uint8_t cache_hit, uint8_t type, uint32_t metadata_in),
+         l2c_prefetcher_cache_fill(uint64_t addr, uint32_t set, uint32_t way, uint8_t prefetch, uint64_t evicted_addr, uint32_t metadata_in),
+         llc_prefetcher_cache_fill(uint64_t addr, uint32_t set, uint32_t way, uint8_t prefetch, uint64_t evicted_addr, uint32_t metadata_in);
     
+    uint32_t get_set(uint64_t address),
+             get_way(uint64_t address, uint32_t set),
+             find_victim(uint32_t cpu, uint64_t instr_id, uint32_t set, const BLOCK *current_set, uint64_t ip, uint64_t full_addr, uint32_t type),
+             llc_find_victim(uint32_t cpu, uint64_t instr_id, uint32_t set, const BLOCK *current_set, uint64_t ip, uint64_t full_addr, uint32_t type),
+             lru_victim(uint32_t cpu, uint64_t instr_id, uint32_t set, const BLOCK *current_set, uint64_t ip, uint64_t full_addr, uint32_t type);
+    
+    void UMON(uint32_t cpu,uint64_t full_addr,uint32_t set),
+         UMON_update(uint32_t cpu, uint32_t set_index, uint32_t way),
+         make_partition(),
+         ucp_update(uint32_t set,uint32_t way,uint32_t cpu);
+    uint32_t UMON_find_victim(uint32_t cpu, uint32_t set_index);
     uint64_t get_utility(uint32_t cpu, uint32_t a);
-
 };
 
 #endif
